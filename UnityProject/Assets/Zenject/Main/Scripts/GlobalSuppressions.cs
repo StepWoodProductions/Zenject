e40338520386b556e845a82953501ea4227bd43d
Alan <Alan-StepWood@users.noreply.github.com>
2016-01-02 11:37:01 -0500

    Fixed issues with Zenject and added "non unity" build types to the project.

---
+++ UnityProject/Assets/Zenject/Main/Scripts/GlobalSuppressions.cs
@@ -0,0 +1,26 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Language", "CSE0003:Use expression-bodied members", Justification = "3rd Party library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0092:Convert field to readonly", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0104:When object creation uses object or collection initializer, empty argument list is redundant", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Redundancies in Code", "RECS0113:Redundant comma in array initializer", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0050:Redundant Where() call with predicate followed by Any()", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0051:Redundant Where() call with predicate followed by Count()", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0053:Redundant Where() call with predicate followed by FirstOrDefault()", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Language Usage Opportunities", "RECS0002:Convert anonymous method to method group", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Language Usage Opportunities", "RECS0091:Use 'var' keyword when possible", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Language Usage Opportunities", "RECS0014:If all fields, properties and methods members are static, the class can be made static.", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0057:Redundant Where() call with predicate followed by Single()", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Language", "CSE0001:Use nameof when passing parameter names as arguments", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0163:Suggest the usage of the nameof operator", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0017:Possible compare of value type with 'null'", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0060:Warns when a culture-aware 'IndexOf' call is used by default.", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0063:Warns when a culture-aware 'StartsWith' call is used by default.", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0133:Parameter name differs in base declaration", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Common Practices and Code Improvements", "RECS0030:Suggests using the class declaring a static function when calling it", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Potential Code Quality Issues", "RECS0133:Parameter name differs in base declaration", Justification = "3rd Party Library")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Code Notifications", "RECS0083:Shows NotImplementedException throws in the quick task bar", Justification = "<Pending>", Scope = "member", Target = "~M:Zenject.BinderUntyped.ToInstance(System.Type,System.Func{System.Object})")]
